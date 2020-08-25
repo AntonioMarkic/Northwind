2ac8b97ff2738511d9f298e7a71bf0ab3f915fa0
AntonioMarkic <44848235+AntonioMarkic@users.noreply.github.com>
2020-08-25 17:12:35 +0200

    Add files via upload

---
+++ MVC-PHP/Model/Entity/Customer.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Customer Entity
+ *
+ * @property string $CustomerID
+ * @property string $CompanyName
+ * @property string|null $ContactName
+ * @property string|null $ContactTitle
+ * @property string|null $Address
+ * @property string|null $City
+ * @property string|null $Region
+ * @property string|null $PostalCode
+ * @property string|null $Country
+ * @property string|null $Phone
+ * @property string|null $Fax
+ */
+class Customer extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'CompanyName' => true,
+        'ContactName' => true,
+        'ContactTitle' => true,
+        'Address' => true,
+        'City' => true,
+        'Region' => true,
+        'PostalCode' => true,
+        'Country' => true,
+        'Phone' => true,
+        'Fax' => true,
+    ];
+}
